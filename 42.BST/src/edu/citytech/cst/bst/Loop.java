51a443d5eb658f419bf4bc1659be4999cbc55a98
jac <joshuacarpentier21@gmail.com>
2019-12-07 04:30:20 -0500

    add

--- 42.BST/src/edu/citytech/cst/bst/Loop.java
+++ 42.BST/src/edu/citytech/cst/bst/Loop.java
@@ -20,34 +20,69 @@ public class Loop {
 
 	public void countTo(int start, int end, int rate, Consumer<Integer> consumer) {
 
-		boolean isReverse = rate < 1;
+//		boolean isReverse = rate < 1;
+//
+//		if (isReverse == false) {
+//			// toggle breakpoint to keep loop from running infinite in the loop
+//
+//			if (end >= start) {
+//				// Base Condition
+//
+//				consumer.accept(start);
+//				// call the method it self inside itself running this would be infinite needs
+//				// condition
+//				start += rate;
+//				countTo(start, end, rate, consumer);
+//			} else
+//				return;
+//		} else {
+//			if (start >= end) {
+//
+//				consumer.accept(start);
+//				start += rate;
+//				countTo(start, end, rate, consumer);
+//			}
+//		
+//		else 
+//			return;
+//		
+//	}
 
-		if (isReverse == false) {
-			// toggle breakpoint to keep loop from running infinite in the loop
-
-			if (end >= start) {
-				// Base Condition
-
-				consumer.accept(start);
-				// call the method it self inside itself running this would be infinite needs
-				// condition
-				start += rate;
-				countTo(start, end, rate, consumer);
-			} else
-				return;
-		} else {
-			if (start >= end) {
-
-				consumer.accept(start);
-				start += rate;
-				countTo(start, end, rate, consumer);
+		if (rate >= 0) {
+			pcountTo(start, end,rate, consumer);
+			}
+		else if (rate < 0) {
+			ncountTo(start, end,rate, consumer);
 			}
 		
-		else 
-			return;
+	}
+
+	
+	
+	private void pcountTo(int start, int end, int rate, Consumer<Integer> consumer) {
+
+			if (end >= start) {
+				consumer.accept(start);
+			
+				start += rate;
+				pcountTo(start, end, rate, consumer);
+			} else
+				return;
 		
-	}
+	
 
 	}
+	
+	private void ncountTo(int start, int end, int rate, Consumer<Integer> consumer) {
 
+		if (start >= end) {
+			consumer.accept(start);
+		
+			start += rate;
+			ncountTo(start, end, rate, consumer);
+		} else
+			return;
+	
+
+	}
 }
